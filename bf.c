d4941ecef6c71d6799c49f957202314e4768d1e2
RemySzpuuk <elijoffee@gmail.com>
2021-01-31 03:37:13 +0000

    :sleep: time to sleep

---
+++ bf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main( int argc, char *argv[] ) {
+ // First argument is filename
+ // second argument is compiler type
+ // third argument is optermised/unoptermised 
+
+ system("");
+ return 0;
+}
---
+++ compile.bat
@@ -0,0 +1,1 @@
+gcc bf.c -o bf.exe
